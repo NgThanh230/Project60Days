6b29bfd75ce4083f47fb09947cfbbbc0fa752871
Nguyen Ba Thanh <ngbathanh2003@gmail.com>
2026-05-26 16:31:15 +0700

    fix upgrade UI

--- Assets/_GlitchSurvival/Scripts/DropRatemanager.cs
+++ Assets/_GlitchSurvival/Scripts/DropRatemanager.cs
@@ -21,19 +21,27 @@ public class DropRateManager : MonoBehaviour
         }
         List<Drops> possibleDrops = new List<Drops>();
         float randomNumber = UnityEngine.Random.Range(0f, 100f);
+
         foreach (Drops rate in drops)
         {
             if (randomNumber <= rate.dropRate)
             {
-               possibleDrops.Add(rate);
+                possibleDrops.Add(rate);
             }
-            if (possibleDrops.Count > 0)
-            {
-                Drops drops = possibleDrops[UnityEngine.Random.Range(0, possibleDrops.Count)];
-                Instantiate(rate.itemPrefab, transform.position, Quaternion.identity);
-            }
-            
         }
+        if (possibleDrops.Count > 0)
+        {
+            // Chọn ngẫu nhiên 1 trong số các món đồ đủ điều kiện rơi
+            Drops selectedDrop = possibleDrops[UnityEngine.Random.Range(0, possibleDrops.Count)];
+            Instantiate(selectedDrop.itemPrefab, transform.position, Quaternion.identity);
+        }
+        //    if (possibleDrops.Count > 0)
+        //    {
+        //        Drops drops = possibleDrops[UnityEngine.Random.Range(0, possibleDrops.Count)];
+        //        Instantiate(rate.itemPrefab, transform.position, Quaternion.identity);
+        //    }
+
+        //}
     }
 
 
